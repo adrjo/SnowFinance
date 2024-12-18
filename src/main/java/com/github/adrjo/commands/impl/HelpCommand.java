b00454523623f649d3d5af60eca97c1e4dfacde7
adrjo <adrianjohansson16@gmail.com>
2024-12-18 14:15:31 +0100

    Increase HelpCommand readability

--- src/main/java/com/github/adrjo/commands/impl/HelpCommand.java
+++ src/main/java/com/github/adrjo/commands/impl/HelpCommand.java
@@ -10,6 +10,8 @@ import com.github.adrjo.commands.menus.MainCommandMenu;
 import com.github.adrjo.commands.menus.SummaryCommandMenu;
 import com.github.adrjo.util.TextColor;
 
+import java.util.Comparator;
+
 @ImplementsMenu(LoggedOutCommandMenu.class)
 @ImplementsMenu(AccountCommandMenu.class)
 @ImplementsMenu(SummaryCommandMenu.class)
@@ -31,8 +33,23 @@ public class HelpCommand extends Command {
 
     @Override
     public void exec(String[] args) {
+        int longest = SnowFinance.instance.getCommandManager().getCommands().stream()
+                .map(Command::getName)
+                .map(String::length)
+                .max(Comparator.comparingInt(l -> l)).get();
+
         for (Command command : SnowFinance.instance.getCommandManager().getCommands()) {
-            System.out.println(TextColor.GREEN + command.getName() + TextColor.RESET + " - " + command.getDesc());
+            int spaces = longest - command.getName().length();
+            String[] lines = command.getDesc().split("\n");
+
+            if (lines.length > 1) {
+                System.out.println(TextColor.GREEN + command.getName() + TextColor.RESET + " ".repeat(spaces) + " - " + lines[0]);
+                for (int i = 1; i < lines.length; ++i) {
+                    System.out.println(" ".repeat(longest + 3) + lines[1]);
+                }
+                continue;
+            }
+            System.out.println(TextColor.GREEN + command.getName() + TextColor.RESET + " ".repeat(spaces) + " - " + command.getDesc());
         }
     }
 }
